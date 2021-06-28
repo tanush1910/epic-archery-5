273284d8231ed22db8e701b0674cca0f98a9f354
tanush1910 <85215220+tanush1910@users.noreply.github.com>
2021-06-28 11:20:02 +0530

    Update ComputerArrow.js

--- ComputerArrow.js
+++ ComputerArrow.js
@@ -9,7 +9,7 @@ class ComputerArrow {
     this.width = width;
     this.height = height;
     this.body = Bodies.rectangle(x, y, this.width, this.height, options);
-    this.image = loadImage("./assets/arrow.png");
+    this.image = loadImage("arrow.png");
     World.add(world, this.body);
   }
 
